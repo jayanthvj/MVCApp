0102ad5beb44db5d2ce857c63842ab5a2111de39
jayanthraj14 <jayanthraj14@users.noreply.github.com>
2020-02-13 18:48:27 +0530

    Add project files.

---
+++ EmployeeRepository/EmployeeRepository.cs
@@ -0,0 +1,25 @@
+﻿using EmployeeEntity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace EmployeeDAL
+{
+    public class EmployeeRepository
+    {
+        public static List<Employee> employee = new List<Employee>();
+         static EmployeeRepository()
+        {
+            employee.Add(new Employee { EmployeeName = "Jayanth", EmployeeId =12, EmployeePhoneNumber = 9086453623,EmployeeDesigination="java" });
+            employee.Add(new Employee { EmployeeName = "Hari", EmployeeId =13, EmployeePhoneNumber = 9500706230 ,EmployeeDesigination=".net"});
+            employee.Add(new Employee { EmployeeName = "Madhu", EmployeeId = 14, EmployeePhoneNumber = 6381517883,EmployeeDesigination="csharp" });
+        }
+        public static IEnumerable<Employee> GetValue()
+        {
+            return employee;
+        }
+    }
+}
---
+++ MvcView/Controllers/EmployeeController.cs
@@ -0,0 +1,22 @@
+﻿using EmployeeDAL;
+using EmployeeEntity;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcView.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult IndexPassing()
+        {
+            IEnumerable<Employee> employee = EmployeeRepository.GetValue();
+            ViewBag.employee = employee;
+            ViewData["employee"] = employee;
+            TempData["employee"] = employee;
+            return View();
+        }
+    }
+}
